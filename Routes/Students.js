ec8041f28e478bf0f398fb0984305a8a1a275de6
Varun Gupta <varun.gupta1798@gmail.com>
2017-06-01 02:29:08 +0530

    complete db
    Signed-off-by: Varun Gupta <varun.gupta1798@gmail.com>

--- Routes/Students.js
+++ Routes/Students.js
@@ -2,9 +2,50 @@
  * Created by varun on 5/24/17.
  */
 const express = require('express');
+const db = require('../Utils/db');
+
+const router = express.Router();
 
 //my router
 
+router.post('/new/:courseId',function (req, res) {
+    if(req.params.courseId){
+
+        db.addStudent(req.body.name,req.body.roll,() => {
+            res.send("Student " + req.body.name + " successfully added");
+        } , req.params.courseId);
+    }
+    else{
+        db.addStudent(req.body.name,req.body.roll,() => {
+            res.send("Student " + req.body.name + " successfully added");
+        });
+    }
 
 
-module.exports = express.Router();
+});
+
+router.get('/:param',function (req, res) {
+    db.searchStudents(req.params.param,(data) => {
+       res.send(JSON.stringify(data));
+    });
+});
+
+router.get('/',function (req, res) {
+    db.getStudents((data) => {
+        res.send(JSON.stringify(data));
+    });
+});
+
+
+
+
+module.exports = router;
+
+
+// db.addCourse("Elixir" , "Arnav Gupta" , new Date() , new Date() , () => {
+//     res.send("Course Added Successfully");
+// })
+
+// db.enrollStudentInCourse(4153 , 1 , ()=>{res.send("success")});
+
+// db.endCourse(1,()=>{res.send("success")});
