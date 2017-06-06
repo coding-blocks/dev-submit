62817866b259dadbae6672f1baef63c3ec778cba
Abhishek Yadav <abhishekgit1208@gmail.com>
2017-06-07 00:36:01 +0530

    add course router

--- Routes/Courses.js
+++ Routes/Courses.js
@@ -5,6 +5,58 @@ const express = require('express');
 
 //Abhishek , your router
 
+const express = require('express');
+const db = require('../Utils/db');
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+
+    let isActive = false;
+    let searchType = "all";
+    let searchParam;
+
+    if (req.query.active) {
+        isActive = true;
+    }
+
+    if (req.query.name) {
+        searchType = "name";
+        searchParam = req.query.name;
+        db.searchCourse(searchParam, searchType, isActive, function (data) {
+            res.send(data);
+        })
+    }
+    //TODO check if ID is in "query"?
+
+    else {
+        db.getCourses(isActive, function (data) {
+            res.send(data);
+        })
+    }
+
+
+})
+
+router.get('/:courseId', function (req, res) {
+    let searchType = "byCourseId";
+    let isActive = false;
+    if (req.query.active) {
+        isActive = true;
+    }
+    db.searchCourse(req.params.courseId, searchType, isActive, function (data) {
+        res.send(data);
+    })
+})
+router.put("/:courseId", function (req, res) {
+    //TODO-add function in db.js
+
+})
+router.post('/new', function (req, res) {
+    db.addCourse(req.query.name, req.query.teacher, req.query.startdate, req.query.enddate, function (data) {
+        res.send(data);
+    });
+})
 
 
 module.exports = express.Router();
