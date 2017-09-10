f78e4695f8dd511d13bd883e53fa5e22e6be0c9e
Abhishek Yadav <abhishekgit1208@gmail.com>
2017-09-10 16:12:07 +0530

    added support for app submission

--- src/utils/config.js
+++ src/utils/config.js
@@ -8,5 +8,6 @@ module.exports = {
     'b336bc467eb51bc8524d3e60c45a86d3c61ba16d857d433085946b51b1d9d9ec',
   BB_client_secret:
     'b0c9f38d85816d04334f9a7ac3d65242f88fdd78f08aaab7a36b5097a1a56f65',
-  BB_token: 'be784fc6747caf4394693aaf797b677865acc776173dc632f5500b6c4f6412da'
+  BB_token: 'be784fc6747caf4394693aaf797b677865acc776173dc632f5500b6c4f6412da',
+  appetize_token:'tok_huyc1m4m8yjuge5nd85fmu6x04'
 };
